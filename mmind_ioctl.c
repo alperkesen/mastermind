0e1f9d69deec81d45cbcc636df4075616c5c6b05
alperkesen <kesen.alper@gmail.com>
2019-11-11 01:25:03 +0300

    Add mmind number argument in ioctl function

--- mmind_ioctl.c
+++ mmind_ioctl.c
@@ -11,21 +11,19 @@ int main(int argc, char *argv[])
     int cmd;
 
     fd = open("/dev/mastermind", O_RDWR);
-
     cmd = atoi(argv[1]);
 
     if (cmd == 0) {
         res = ioctl(fd, MMIND_REMAINING, 0);
 	printf("Remaining guess: %d\n", res);
     } else if (cmd == 1) {
-      res = ioctl(fd, MMIND_NEWGAME, 0);
-      printf("Started new game! (MN: %s)\n",
-	     argv[2]);
+        res = ioctl(fd, MMIND_NEWGAME, atoi(argv[2]));
+        printf("Started new game! (MN: %s)\n", argv[2]);
     } else if (cmd == 2) {
-      res = ioctl(fd, MMIND_ENDGAME, 0);
-      printf("End game!\n");
+        res = ioctl(fd, MMIND_ENDGAME, 0);
+        printf("End game!\n");
     } else {
-      printf("Wrong command!\n");
+        printf("Wrong command!\n");
     }
 
     return 0;
