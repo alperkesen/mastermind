0c6ab6621456a2088c7138c6915916171492e755
alperkesen <kesen.alper@gmail.com>
2019-11-10 20:59:52 +0300

    Change error return after exceeding max guesses

--- mastermind.c
+++ mastermind.c
@@ -215,9 +215,8 @@ ssize_t mastermind_write(struct file *filp, const char __user *buf, size_t count
 
     dev->current_guess++;
 
-
     if (dev->current_guess > mmind_max_guesses) {
-        retval = -EFAULT;
+        retval = -EDQUOT;
         goto out;
     }
 
